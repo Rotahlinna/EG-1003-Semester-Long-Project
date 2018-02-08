16592faf9ca65de0fb2a081c5656ea3d4805096b
John Cunniff <johncunniff1248@gmail.com>
2018-02-08 12:55:11 -0500

    CHG

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    public int maxPlatforms = 20;
+    public GameObject platform;
+    public float horizontalMin = 6.5f;
+    public float horizontalMax = 14f;
+    public float verticalMin = -6f;
+    public float verticalMax = 6f;
+
+    private Vector2 originPosition;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        originPosition = transform.position;
+        Spawn();
+	}
+	
+	void Spawn()
+    {
+        for (int i = 0; i < maxPlatforms; i++)
+        {
+            Vector2 randomPosition = originPosition + new Vector2(Random.Range(horizontalMin, horizontalMax), Random.Range(verticalMin, verticalMax));
+            Instantiate(platform, randomPosition, Quaternion.identity);
+            originPosition = randomPosition;
+        }
+    }
+}
